302f8ab2507e57f6a284565ce989f4cfa7768f0c
R_hoya <niko98722@gmail.com>
2026-06-12 17:35:17 +0900

    0612(Fri)進捗

---
+++ ArduinoIDE.0612ver/robot_pet/weather_task.h
@@ -0,0 +1,17 @@
+#ifndef WEATHER_TASK_H
+#define WEATHER_TASK_H
+ 
+#include <Arduino.h>
+#include "eye_animation.h"
+ 
+#define WEATHER_INTERVAL_MS  600000UL   // 10分ごと
+#define WEATHER_RETRY_MS      30000UL   // 失敗時リトライ間隔
+ 
+void        weatherTaskInit();
+EmotionType weatherTaskUpdate();
+ 
+int         weatherGetId();
+float       weatherGetTemp();
+const char* weatherGetDesc();
+ 
+#endif // WEATHER_TASK_H
